bdaab608f7babd549b074ac552b4072f3172ccd1
Oksana Vorobei <oks.vorobei@ukr.net>
2025-09-05 16:45:32 +0300

    add js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,41 @@
-console.log('Form');
+let formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+const feedbackFormStorageData = localStorage.getItem('feedback-form-state');
+if (feedbackFormStorageData) {
+  formData = JSON.parse(feedbackFormStorageData);
+  Object.keys(formData).forEach(key => {
+    let value = formData[key];
+    form.querySelector(`[name="${key}"`).value = value;
+  });
+}
+const key = 'feedback-form-state';
+
+form.addEventListener('input', handlerInput);
+
+function handlerInput(event) {
+  let field = event.target;
+  formData[field.name] = field.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  let errors = 0;
+  Object.keys(formData).forEach(key => {
+    let value = formData[key];
+    if (!value) {
+      errors++;
+    }
+  });
+  if (errors) {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+  }
+}
